0a19a68865fdf53e7d1f507316d68a8b67ee5073
muyeen-khan <muyeenkhan80@gmail.com>
2024-03-12 13:17:21 +0600

    last

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="h-screen bg-green-200 text-green-500 text-9xl flex items-center justify-center">
+      Tanver Mohammad
+    </h1>
+  );
+}
